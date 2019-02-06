332b68ccad15009cc54a1e66a35a42caa6a61371
Alex <alex.thepaut33@gmail.com>
2019-02-06 14:44:13 +0100

    Ajout des fonctionnalités "ajout", "modifier" et "suppression" de pizza

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -39,18 +39,66 @@ public class PizzeriaAdminConsoleApp {
 				
 				for(int i = 0; i < pizzas.length; i++){
 					System.out.println(pizzas[i].code + " -> " + pizzas[i].libelle + " (" + pizzas[i].prix + ")");
-					System.out.println("");
 				}
+				System.out.println("");
 				
 				break;
 			case 2: 
 				System.out.println("Ajout d'une nouvelle pizza");
+				
+				Pizza[] pizzasTemp = new Pizza[pizzas.length+1];
+				for(int i=0; i < pizzas.length; i++){
+					pizzasTemp[i] = pizzas[i];
+				}
+				
+				System.out.println("Veuiler saisir le code : ");
+				String codeTemp = choixUtilisateur.next();
+				System.out.println("Veuiler saisir le nom (sans espace) : ");
+				String nomTemp = choixUtilisateur.next();
+				System.out.println("Veuiler saisir le prix : ");
+				Double prixTemp = Double.parseDouble(choixUtilisateur.next());
+				
+				pizzasTemp[pizzas.length] = new Pizza(pizzas.length, codeTemp, nomTemp, prixTemp);
+				pizzas = pizzasTemp;
+				
 				break;
 			case 3: 
+				
 				System.out.println("Mise a jour d'une pizza");
+				
+				System.out.println("Veuiller choisir le code de la pizza à modifier : ");
+				String codeModifTempUtilisateur = choixUtilisateur.next();
+				
+				int idFind = -1;
+				for(int i = 0; i < pizzas.length; i++){
+					if(pizzas[i].code.equals(codeModifTempUtilisateur)){
+						idFind = i;
+						break;
+					}
+				}
+				
+				System.out.println("Veuiler saisir le nouveau code : ");
+				pizzas[idFind].code = choixUtilisateur.next();
+				System.out.println("Veuiler saisir le nouveau nom (sans espace) : ");
+				pizzas[idFind].libelle = choixUtilisateur.next();
+				System.out.println("Veuiler saisir le nouveau prix : ");
+				pizzas[idFind].prix = Double.parseDouble(choixUtilisateur.next());
+				
 				break;
 			case 4: 
 				System.out.println("Suppression d'une pizza");
+				System.out.println("Veuiler choisir le code de la pizza a supprimer : ");
+				String codeSupTemp = choixUtilisateur.next();
+				
+				Pizza[] pizzasSupTemp = new Pizza[pizzas.length-1];
+				for(int i=0, j=0; i < pizzas.length; i++){
+					if(!pizzas[i].code.equals(codeSupTemp)){
+						pizzasSupTemp[j] = pizzas[i];	
+						j++;
+					}
+				}
+				pizzas = pizzasSupTemp;
+				
 				break;
 			case 99: 
 			default: 
