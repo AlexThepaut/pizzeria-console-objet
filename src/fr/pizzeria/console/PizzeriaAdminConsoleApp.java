8081586ebdcc7b99c1eeb147f0e82e45c739c731
Alex <alex.thepaut33@gmail.com>
2019-02-06 12:38:58 +0100

    Ajout de l'init Pizza
    Et modifs mineurs

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,18 +1,33 @@
 package fr.pizzeria.console;
 
 import java.util.Scanner;
+import fr.pizzeria.model.*;
 
 public class PizzeriaAdminConsoleApp {
 	
+	private static Pizza[] pizzas = new Pizza[8];
+	
+	
 	public static void main(String[] args) {
+		
+		pizzas[0] = new Pizza(0, "PEP", "Peperoni", 12.50f);
+		pizzas[1] = new Pizza(1, "MAR", "Margherita", 14.00f);
+		pizzas[2] = new Pizza(2, "REIN", "La Reine", 11.50f);
+		pizzas[3] = new Pizza(3, "FRO", "La 4 fromages", 12.00f);
+		pizzas[4] = new Pizza(4, "CAN", "La cannibale", 12.50f);
+		pizzas[5] = new Pizza(5, "SAV", "La savoyarde", 13.00f);
+		pizzas[6] = new Pizza(6, "ORI", "L'orientale", 13.50f);
+		pizzas[7] = new Pizza(7, "IND", "L'indienne", 14.00f);
+		
 		Scanner choixUtilisateur = new Scanner(System.in);
+		
 		boolean fermeture = false;
 		
 		while(fermeture != true){
 			System.out.println("***** Pizzeria Administration *****");
 			System.out.println("1. Lister les pizzas");
 			System.out.println("2. Ajouter une nouvelle pizza");
-			System.out.println("3. Mettre � jour une pizza");
+			System.out.println("3. Mettre a jour une pizza");
 			System.out.println("4. Supprimer une pizza");
 			System.out.println("99. Sortir");
 			
@@ -38,5 +53,6 @@ public class PizzeriaAdminConsoleApp {
 				break;
 			}
 		}
+		choixUtilisateur.close();
 	}
 }
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -7,6 +7,16 @@ public class Pizza {
 	public String libelle;
 	public double prix;
 	
+	public Pizza(){
+		
+	}
+	
+	public Pizza(String code, String libelle, double prix){
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	
 	public Pizza(int id, String code, String libelle, double prix){
 		this.id = id;
 		this.code = code;
